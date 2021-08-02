942c30ea7e9a2fa54b8853245e0f232b87529c24
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2021-08-02 11:27:07 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "KKM site",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
