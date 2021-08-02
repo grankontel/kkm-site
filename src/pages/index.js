a4f08731693232c25f8c0bdfbbaff99dfb18adfa
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2021-08-02 15:06:43 +0200

    basic index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,32 @@ import React from 'react'
 import { Columns, Container, Content, Heading } from 'react-bulma-components'
 import Navigation from '../components/navigation'
 import Page from '../layouts/page'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
-    return (
-        <Page>
-            <Navigation />
-            <Container renderAs="main" role="main" pt={6}>
-                <Columns>
-                <Columns.Column size="three-fifths">
-
-                </Columns.Column>
-                <Columns.Column size="two-fifths">
-                    <Heading>Korijé Kréyòl Maké</Heading>
-                    <Content>
-                        Tout zouti ou bizwen pou korijé kréyòl maké
-                    </Content>
-                </Columns.Column>
-                </Columns>
-            
-            </Container>
-            
-        </Page>
-    )
+  return (
+    <Page>
+      <Navigation />
+      <Container renderAs="main" role="main" pt={6}>
+        <Columns>
+          <Columns.Column size="three-fifths">
+            <StaticImage
+              src="../images/alfons-morales-bibliotek.jpg"
+              placeholder="blurred"
+              layout="fixed"
+              width={590}
+              height={260}
+              alt="Bibliotek"
+            />
+          </Columns.Column>
+          <Columns.Column size="two-fifths">
+            <Heading>Korijé Kréyòl Maké</Heading>
+            <Content>Tout zouti ou bizwen pou korijé kréyòl maké</Content>
+          </Columns.Column>
+        </Columns>
+      </Container>
+    </Page>
+  )
 }
 
 export default IndexPage
