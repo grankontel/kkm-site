188ed65ade8fa04aeb0115a883ba4b5923dab258
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2021-08-02 14:55:20 +0200

    basic layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Columns, Container, Content, Heading } from 'react-bulma-components'
+import Navigation from '../components/navigation'
 import Page from '../layouts/page'
 
 const IndexPage = () => {
     return (
         <Page>
-            <h1>This is a test</h1>
+            <Navigation />
+            <Container renderAs="main" role="main" pt={6}>
+                <Columns>
+                <Columns.Column size="three-fifths">
+
+                </Columns.Column>
+                <Columns.Column size="two-fifths">
+                    <Heading>Korijé Kréyòl Maké</Heading>
+                    <Content>
+                        Tout zouti ou bizwen pou korijé kréyòl maké
+                    </Content>
+                </Columns.Column>
+                </Columns>
+            
+            </Container>
+            
         </Page>
     )
 }
