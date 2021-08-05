3eea10c72554bd5bdc75db382d8e272056e1380c
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2021-08-05 19:29:26 +0200

    ProjectList component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
 import {
-  Card,
   Columns,
   Container,
   Content,
@@ -13,84 +11,8 @@ import Page from '../layouts/page'
 import { StaticImage } from 'gatsby-plugin-image'
 import { FaTwitter, FaGithub } from 'react-icons/fa'
 import Seo from '../components/seo'
-const classNames = require('classnames')
+import ProjectList from '../components/projectList'
 
-const ProjectList = () => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query MyQuery {
-          projects: allProjectsJson {
-            edges {
-              node {
-                id
-                title
-                description
-                link
-                repo
-              }
-            }
-          }
-        }
-      `}
-      render={(data) => {
-        const projects = data.projects.edges
-        return (
-          <Columns>
-            {projects.map((project, index) => (
-              <ProjectItem key={index} item={project.node} />
-            ))}
-          </Columns>
-        )
-      }}
-    />
-  )
-}
-const ProjectItem = ({ item }) => {
-  const btnClass = classNames({
-    buttons: true,
-    'is-right': true,
-    'mt-4': true,
-  })
-
-  return (
-    <Columns.Column size="one-third">
-      <Card>
-        <Card.Content>
-          <Heading size={5} textFamily="secondary" renderAs="h3">
-            {item.title}
-          </Heading>
-          <Content>
-            <p className="text-medium rem-80">{item.description}</p>
-          </Content>
-          <div className={btnClass}>
-            {item.repo && (
-              <a
-                className="button is-small is-light"
-                href={item.repo}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <span className="icon">
-                  <FaGithub />
-                </span>
-                <span>GitHub</span>
-              </a>
-            )}
-            <a
-              className="is-accent is-small  button"
-              href={item.link}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Vizité
-            </a>
-          </div>
-        </Card.Content>
-      </Card>
-    </Columns.Column>
-  )
-}
 const IndexPage = () => {
   return (
     <Page>
